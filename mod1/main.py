cf886185cd0363aa5e154d7da53d5f57ebcf9111
Anton <tongwynbleidd1459@gmail.com>
2025-05-14 22:25:06 +0500

    Fix mod1

--- mod1/main.py
+++ mod1/main.py
@@ -1,58 +1,74 @@
 import os
 import random
 import re
-from flask import Flask
 from datetime import datetime, timedelta
+from flask import Flask
 
 app = Flask(__name__)
 
-cars = ['Chevrolet', 'Renault', 'Ford', 'Lada']
-cats = ['корниш-рекс', 'русская голубая', 'шотландская вислоухая', 'мейн-кун', 'манчкин']
-words = []
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-BOOK_FILE = os.path.join(BASE_DIR, 'war_and_peace.txt')
-counter = 0
-@app.route('/')
-def index():
-    return '<h1>main_empty_page</h1>'
-
+# -------------------------------------
+# Задача 1. /hello_world
 @app.route('/hello_world')
 def hello_world_view():
-    return f'<h1>Привет, мир!</h1>'
+    return 'Привет, мир!'
 
+# -------------------------------------
+# Задача 2. /cars
+cars = [
+    'Chevrolet',
+    'Renault',
+    'Ford',
+    'Lada'
+    ]
 
 @app.route('/cars')
 def cars_view():
-    return f'<p>{", ".join(cars)}</p>'
+    return f'{", ".join(cars)}'
 
+# -------------------------------------
+# Задача 3. /cats
+cats = [
+    'корниш-рекс',
+    'русская голубая',
+    'шотландская вислоухая',
+    'мейн-кун',
+    'манчкин'
+    ]
 
 @app.route('/cats')
 def cats_view():
     choice = random.choice(cats)
-    return f'<p>{choice}</p>'
+    return f'{choice}'
 
-
+# -------------------------------------
+# Задача 4. /get_time/now
 @app.route('/get_time/now')
 def get_time_now_view():
     current_datetime = datetime.now()
     current_time = current_datetime.strftime('%H-%M-%S')
-    return f'<p>Точное время: {current_time}</p>'
+    return f'Точное время: {current_time}'
 
-
+# -------------------------------------
+# Задача 5. /get_time/future
 @app.route('/get_time/future')
 def get_time_future_view():
     current_datetime = datetime.now()
     delta = timedelta(hours=1)
     future_datetime = current_datetime + delta
     future_time = future_datetime.strftime('%H-%M-%S')
-    return f'<p>Точное время: {future_time}</p>'
+    return f'Точное время через час будет: {future_time}'
 
+# -------------------------------------
+# Задача 6. /get_random_word
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BOOK_FILE = os.path.join(BASE_DIR, 'war_and_peace.txt')
+words = []
 
 def make_words():
     with open(BOOK_FILE, encoding='utf-8') as file:
         text = file.read()
         global words
-        words = re.findall(r'\b[а-яА-Я]+\b', text)
+        words = re.findall(r'\b[a-zA-Zа-яА-Я]+\b', text)
 
 
 @app.route('/get_random_word')
@@ -60,18 +76,16 @@ def get_random_word_view():
     if not words:
         make_words()
     word_random = random.choice(words)
-    return f'<p>{word_random}</p>'
+    return f'{word_random}'
 
-
+# -------------------------------------
+# Задача 7. /counter
 @app.route('/counter')
 def counter():
     counter.visits += 1
 
-    return f'<p>{counter.visits}</p>'
-
-
-counter.visits = 0
-
+    return f'{counter.visits}'
 
 if __name__ == '__main__':
+    counter.visits = 0
     app.run(debug=True)
