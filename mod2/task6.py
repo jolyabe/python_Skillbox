0989f52ec517159023545a0fdaa96f789f49a4fc
Anton <tongwynbleidd1459@gmail.com>
2025-05-28 23:25:41 +0500

    Fix task6 in mod2

--- mod2/task6.py
+++ mod2/task6.py
@@ -3,19 +3,23 @@
 from flask import Flask
 import os
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 app = Flask(__name__)
 
 @app.route("/head_file/<int:size>/<path:relative_path>")
 def head_file(size: int, relative_path: str):
+
+    abs_path = os.path.join(BASE_DIR, relative_path)
+
     try:
-        abs_path = os.path.abspath(relative_path)
-        with open(relative_path, encoding='utf-8') as file:
+        with open(abs_path, encoding='utf-8') as file:
             result_text = file.read(size)
             result_size = len(result_text)
 
     except FileNotFoundError:
         return 'file not found'
-    
+
     return f'<b>{abs_path}</b> {result_size}<br>{result_text}'
 
 
